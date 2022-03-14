7fe8dc58b452963f9eaafd4853fea5a5b5c86f5d
Adaobiokeke <adaobiotiji@yahoo.com>
2022-03-14 12:12:32 -0400

    error-page

--- src/Components/Error/error.js
+++ src/Components/Error/error.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './error.css'
 
 
 function Error() {
   return (
-    <div className="homepage">Error</div>
+    <div className="error">
+      <h2>
+      OOOPS! <br/>
+      Wrong Page! You are either using a broken path or your system is broken!!!
+      </h2>
+
+      </div>
   )
 }
 
